835ee27d41f57a6901f4c441c51e9f3f2dc34988
aaminahasl1451 <152324769+aaminahasl1451@users.noreply.github.com>
2024-01-29 19:28:09 +0000

    done

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mySiteA4 project.
+URL configuration for mySiteA5 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
